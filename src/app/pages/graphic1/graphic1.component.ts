74aa46dc3386f0c9101ca803a5214a83702b3b26
Alejo Tapias <alejotapias0414@gmail.com>
2020-10-26 22:21:05 -0500

    Input, output y viewchild

--- src/app/pages/graphic1/graphic1.component.ts
+++ src/app/pages/graphic1/graphic1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-graphic1',
   templateUrl: './graphic1.component.html',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Graphic1Component implements OnInit {
 
+ 
+  labels1:string[] = ['TITULO 1', 'In-Store Sales', 'Mail-Order Sales'];
+
+  data1 = [
+    [10, 20, 30],
+  ];
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
