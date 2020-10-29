cf4739c5ffa3f65a0f6efc86b7224890d0da246b
Alejo Tapias <alejotapias0414@gmail.com>
2020-10-29 16:31:50 -0500

    Observables,promesas&rxjsOperators

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import {filter, map} from 'rxjs/operators'
+
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.css']
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  title:string ='';
+  tituloSubs$: Subscription;
+
+  constructor(private router:Router) {
+    this.tituloSubs$=this.getArgumentosRuta()
+      .subscribe(({title}) => {
+        this.title=title;
+        document.title=`AdminPro-${title}`;
+      }) 
+   }
 
   ngOnInit(): void {
   }
+
+  getArgumentosRuta(){
+
+    return this.router.events.pipe(
+      filter( event => event instanceof ActivationEnd),
+      filter( (event:ActivationEnd) => event.snapshot.firstChild===null),
+      map( (event:ActivationEnd) => event.snapshot.data))
+      // 
+  }
+
+  ngOnDestroy(): void {
+    // Esto lo hicimios porque al momento de darle logout, ya no es neesario saber la informacion que contiene la ruta.
+    this.tituloSubs$.unsubscribe();
+  }
+
+
+
+
 
 }
